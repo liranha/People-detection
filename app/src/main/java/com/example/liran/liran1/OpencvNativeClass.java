5f1bd1ca50db19a46bed7eeff3248b284ccea8f2
liranha <liranhazek@gmail.com>
2016-11-11 19:16:14 +0200

    Delete OpencvNativeClass.java

--- app/src/main/java/com/example/liran/liran1/OpencvNativeClass.java
+++
@@ -1,9 +0,0 @@
-package com.example.liran.liran1;
-
-/**
- * Created by liran on 15/10/2016.
- */
-
-public class OpencvNativeClass {
-    public native static int convertGray(long matAddrRgba, long matAddrGray);
-}
